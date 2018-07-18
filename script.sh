a1a2cd364676919938c30ceeeee36761f129d844
Shivam Agrawal <shivam@Shivams-Macbook.local>
2018-07-18 11:30:58 +0530

    add lines to run the container1

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ docker build -t devops0309/devops:travis1 .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push devops0309/devops:travis1
 docker pull devops0309/devops:travis1
-docker run --name nginx devops0309/devops:travis1 -c "sudo service nginx start"
+docker run --name nginx devops0309/devops:travis1 /bin/bash -c "sudo service nginx start"
 
