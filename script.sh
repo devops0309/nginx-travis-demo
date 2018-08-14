b6ff1d61314f4663a49015f1664d14745ce21625
Shivam Agarwal <shivam2.agarwal@paytmmall.com>
2018-08-14 10:20:37 +0530

    add sleep to check the timeout

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 echo "doing step 1"
 echo -ne '\n' | sudo add-apt-repository ppa:ethereum/ethereum
 echo "doing step 2"
+sleep 900;
 sudo apt-get -y update
 echo "doing step 3"
 sudo apt-get -y install solc
