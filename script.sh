670f479894ddff6d53ea091041fec4225194bf30
Shivam Agarwal <shivam2.agarwal@paytmmall.com>
2018-08-14 09:58:26 +0530

    change script to install solc

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/devops0309/nginx-travis-demo.git
-
+#git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/devops0309/nginx-travis-demo.git
+echo "doing step 1"
+echo -ne '\n' | sudo add-apt-repository ppa:ethereum/ethereum
+echo "doing step 2"
+sudo apt-get -y update
+echo "doing step 3"
+sudo apt-get -y install solc
+which solc
